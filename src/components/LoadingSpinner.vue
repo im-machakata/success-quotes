79ff452cdc6c2f1a38e80968ec0b52e60e6bed14
mimachakata <mimachakata@gmail.com>
2024-11-29 02:23:48 +0000

    refactor: min height for loading spinner

--- src/components/LoadingSpinner.vue
+++ src/components/LoadingSpinner.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class='flex space-x-2 justify-center items-center min-h-[80vh]'>
+    <div class='flex space-x-2 justify-center items-center min-h-[75vh]'>
         <span class='sr-only'>Loading...</span>
         <div class='h-8 w-8 bg-green-200 rounded-full animate-bounce [animation-delay:-0.3s]'></div>
         <div class='h-8 w-8 bg-green-200 rounded-full animate-bounce [animation-delay:-0.15s]'></div>
