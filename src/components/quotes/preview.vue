5fe6fb9edd453dc422128f622273941c96d2e929
Isaac Machakata <hie@isaac.co.zw>
2024-12-30 03:34:40 +0000

    refactor: rename quotes preview component and enlarge font size

--- src/components/quotes/preview.vue
+++ src/components/quotes/preview.vue
@@ -18,18 +18,15 @@ const isHome = route.fullPath == "/";
 </script>
 
 <template>
-    <div class="quote" :class="{
+    <NuxtLink :href="(isHome ?'/quote/' + quoteId : '')" class="quote" :class="{
         'select-all': !isHome,
-        'cursor-pointer': isHome
-    }">
-        <p class="text-gray-700">
-            <q class="line-clamp-3 lg:line-clamp-4">{{ text }}</q>
-        </p>
-        <p class="text-gray-500 text-sm mt-2 line-clamp-1">&ndash; {{ author }}</p>
-    </div>
+    }" >
+        <q class="text-gray-700 line-clamp-3 lg:text-lg">{{ text }}</q>
+        <p class="text-gray-500 mt-2 line-clamp-1">&ndash; {{ author }}</p>
+    </NuxtLink>
 </template>
 
-<style>
+<style scoped>
 .quote {
     @apply bg-white p-4 rounded-lg shadow border-2 border-green-600 flex flex-col justify-between;
 }
