c7cdb3d073a23e7877a514b75120b524f398a0e6
Isaac Machakata <hie@isaac.co.zw>
2024-12-31 04:04:36 +0000

    feat: add quote icon

---
+++ src/components/quotes/icon.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const props = defineProps({
+    url: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div class="quote-icon">
+        <LazyNuxtImg :src="props.url" height="50" width="50" :alt="url"/>
+    </div>
+</template>
+
+<style scoped>
+.quote-icon {
+    @apply bg-white rounded-lg shadow border-2 border-green-600 flex flex-col justify-center p-2;
+}
+img {
+    @apply h-6 w-6
+}
+</style>
