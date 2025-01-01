a13808cbdd4110b195e5d199c03b7b8efa2c6c78
Isaac Machakata <hie@isaac.co.zw>
2025-01-01 01:13:15 +0000

    style: make quote icon transluscent

--- src/components/quotes/icon.vue
+++ src/components/quotes/icon.vue
@@ -15,7 +15,7 @@ const props = defineProps({
 
 <style scoped>
 .quote-icon {
-    @apply bg-white rounded-lg shadow border-2 border-green-600 flex flex-col justify-center p-2;
+    @apply bg-white/90 rounded-lg shadow border-2 border-green-600 flex flex-col justify-center p-2;
 }
 img {
     @apply h-6 w-6
