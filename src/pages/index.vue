2b2224bcef3ff0d2cf704373e5206275559e4135
Isaac Machakata <hie@isaac.co.zw>
2025-01-01 01:42:03 +0000

    feat: load more quotes and show error message when no new quotes

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,6 +3,7 @@ const quote = ref({});
 const quotes = ref([]);
 const toastMessage = ref('');
 const quoteToggled = ref(false);
+const loadingQuotes = ref(true);
 const runtimeConfig = useRuntimeConfig();
 const { database, Query } = useAppwrite();
 
@@ -22,20 +23,47 @@ const showCopiedMessage = e => {
     }, 5000);
 }
 
-const dbConnection = database.listDocuments(
-    runtimeConfig.public.database,
-    runtimeConfig.public.quotesTable, 
-    [ 
+const loadQuotes = lastId => {
+    const filter = [
         Query.orderAsc('text'),
         Query.limit(24),
-    ]
-);
+    ];
+    if (lastId) filter.push(Query.offset(lastId));
+    return database.listDocuments(
+        runtimeConfig.public.database,
+        runtimeConfig.public.quotesTable,
+        filter
+    );
+}
 
-onMounted(() => {
-    dbConnection.then(function (response) {
-        quotes.value = response.documents;
+const loadMore = () => {
+    loadingQuotes.value = true;
+    loadQuotes(quotes.value.length).then(function (response) {
+        if (response.documents.length === 0) {
+            toastMessage.value = 'No new quotes available.';
+            setTimeout(() => {
+                toastMessage.value = '';
+            }, 5000);
+            loadingQuotes.value = false;
+            return;
+        } else {
+            quotes.value = [...quotes.value, ...response.documents];
+        }
+
+        loadingQuotes.value = false;
     }, function (error) {
         console.log(error);
+        loadingQuotes.value = false;
+    });
+}
+
+onMounted(() => {
+    loadQuotes().then(function (response) {
+        quotes.value = response.documents;
+        loadingQuotes.value = false;
+    }, function (error) {
+        console.log(error);
+        loadingQuotes.value = false;
     });
 });
 
@@ -48,11 +76,18 @@ useSeoMeta({
 <template>
     <div class="quotes-container mx-auto p-4">
         <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 py-4">
-            <spinners-loading v-if="!quotes.length" class="col-span-5" />
-            <lazy-quotes-preview :quote="quote_item" v-for="quote_item in quotes" :key="quote_item.$id" @open-quote="openQuote" />
+            <spinners-loading v-show="loadingQuotes" class="col-span-5" />
+            <lazy-quotes-preview v-show="!loadingQuotes" :quote="quote_item" v-for="quote_item in quotes"
+                :key="quote_item.$id" @open-quote="openQuote" />
         </div>
-        <lazy-quotes-view v-show="quoteToggled" @close-quote="closeQuote" :quote="quote" @quote-copied="showCopiedMessage" />
-        <lazy-alerts v-show="toastMessage" :message="toastMessage"/>
+        <lazy-quotes-view v-show="quoteToggled" @close-quote="closeQuote" :quote="quote"
+            @quote-copied="showCopiedMessage" />
+        <lazy-alerts v-show="toastMessage" :message="toastMessage" />
+        <div class="flex items-center justify-center">
+            <NuxtLink v-show="!loadingQuotes"
+                class="border-2 border-green-600 bg-white p-2 rounded-lg mb-4 px-4 cursor-pointer text-sm"
+                @click.prevent="loadMore">More Quotes</NuxtLink>
+        </div>
     </div>
 </template>
 
@@ -70,6 +105,6 @@ useSeoMeta({
 }
 
 ::-webkit-scrollbar-thumb {
-    @apply bg-green-400 hover:bg-green-500;
+    @apply bg-green-400 hover:bg-green-500 rounded;
 }
 </style>
