f59f034178b1a0e92ca4c103973660c496f0af92
Machakata Isaac <isaac@caasi.co.zw>
2025-01-03 20:21:14 +0000

    refactor: add adsense verification

--- src/app.vue
+++ src/app.vue
@@ -2,7 +2,8 @@
 useHead({
   meta: [
     { name: 'author', content: 'Isaac Machakata' },
-    { name: 'keywords', content: 'success quotes, motivational quotes, business quotes'}
+    { name: 'keywords', content: 'success quotes, motivational quotes, business quotes'},
+    { name: "google-adsense-account", content: "ca-pub-6238806291448360" }
   ],
   link: [
     { rel: 'icon', href: 'https://img.icons8.com/?size=100&id=61893&format=png&color=239d53', type: 'image/png' }
